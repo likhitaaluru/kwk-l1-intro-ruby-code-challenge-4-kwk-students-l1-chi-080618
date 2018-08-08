d5e93156e289de55a9af8e07e4efbe2cfa0b240d
likhitaaluru <likhitaaluru@gmail.com>
2018-08-08 20:52:08 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,11 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
+least_coins={
+  :quarters => "0.25"
+  :dimes => "0.10"
+  :nickels => "0.05"
+  :pennies => "0.01"
+}
 
 end
