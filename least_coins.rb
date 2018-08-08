53ed05d67bffca292227a5cc99c57785e7d3b9b7
likhitaaluru <likhitaaluru@gmail.com>
2018-08-08 21:15:41 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,10 +2,10 @@
 
 def least_coins(cents)
 coins={
-  :quarters => "0"
-  :dimes => "0"
-  :nickels => "0"
-  :pennies => "0"
+  :quarters => 0
+  :dimes => 0
+  :nickels => 0
+  :pennies => 0
 }
 until cents==0
   if cents%25==0
